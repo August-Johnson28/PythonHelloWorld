158e128d29fb8473741d38c2fb14be33ec02b406
August-Johnson28 <bionicaugust@gmail.com>
2024-09-24 21:48:49 +0000

    Update hello_world.py by asking user for name

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,13 @@
-
+"""
+This is my first program. It outputs Hello World! to the screen.
+August Johnson - September 2024
+"""
 
 
 def main():
-  pass
-
-
+    name: str = input("What is your name?")
+    print("Hello", name, "!")
 
 if __name__ == "__main__":
   main()
+  
